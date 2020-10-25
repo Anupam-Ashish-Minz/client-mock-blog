635320a58a65f5b19553a996a33fae62d5ee9207
Anupam Ashish Minz <anupamminz300am@gmail.com>
2020-10-25 19:55:22 +0530

    fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import './navbar.css'
 const Navbar = () => {
   return (
     <div className="navbar-container">
-      <Link to="/home">Home</Link> 
+      <Link to="/">Home</Link> 
       <Link to="/about">About</Link>
       <Link to="/articles">Articles</Link>
     </div>
